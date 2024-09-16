d7071b481514357fd73f1693174ec1ecc587ed66
안덕우 <fjrlvkdlxj@naver.com>
2024-09-16 18:30:17 +0900

    2024.09.16

--- Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Publisher & Subscriber.xcplaygroundpage/Contents.swift
@@ -4,8 +4,27 @@ import Foundation
 import Combine
 
 // Publisher & Subscriber
+let just = Just(1000)
+let subscription = just.sink { value in
+    print("Recived Value \(value)")
+}
 
+let arrayPublisher = [1, 3, 5, 7, 9].publisher
+let subscription2 = arrayPublisher.sink { value in
+    print("Recived Value \(value)")
+}
 
+class MyClass {
+    var property: Int = 0 {
+        didSet {
+            print("Did set property to \(property)")
+        }
+    }
+}
+
+let object = MyClass()
+let subscription3 = arrayPublisher.assign(to: \.property, on: object)
+print("Final Vlaue \(object.property)")
 
 
 
--- Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Subscription.xcplaygroundpage/Contents.swift
@@ -3,10 +3,20 @@
 import Foundation
 import Combine
 
-
-
+let subject = PassthroughSubject<String, Never>()
 
 // The print() operator prints you all lifecycle events
+let subcription = subject
+    .print("[Debug]")
+    .sink { value in
+    print("Subcriber received value \(value)")
+}
 
+subject.send("Hello")
+subject.send("Hello again!")
+subject.send("Hello for the last tiem")
+//subject.send(completion: .finished)
+subcription.cancel()
+subject.send("Hello ?? :(")
 
 //: [Next](@next)
