342e3c0efa2fd4dcb86f3f047fd5756f26f504a3
안덕우 <fjrlvkdlxj@naver.com>
2024-09-22 02:13:54 +0900

    2024.09.22

--- Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Scheduler.xcplaygroundpage/Contents.swift
@@ -3,7 +3,18 @@
 import Foundation
 import Combine
 
+let arrPublisher = [1, 2, 3].publisher
 
+let queue = DispatchQueue(label: "custom")
+
+let subscription = arrPublisher
+    .map { value -> Int in
+        print("transform: \(value), thread: \(Thread.current)")
+        return value
+    }
+    .sink { value in
+    print("Receive Value: \(value), thread: \(Thread.current)")
+}
 
 
 
