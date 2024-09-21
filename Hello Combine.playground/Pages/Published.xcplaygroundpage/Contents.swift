95e49ef9962484319a14a0d54a21925651786a6e
안덕우 <fjrlvkdlxj@naver.com>
2024-09-21 23:12:56 +0900

    2024.09.21

--- Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
+++ Hello Combine.playground/Pages/Published.xcplaygroundpage/Contents.swift
@@ -4,6 +4,23 @@ import Foundation
 import UIKit
 import Combine
 
+final class SomeViewModel {
+    @Published var name = "Jack"
+    var age = 20
+}
+
+final class Label {
+    var text = ""
+}
+
+let label = Label()
+let vm = SomeViewModel()
+print("text: \(label.text)")
+
+vm.$name.assign(to: \.text, on: label)
+print("text: \(label.text)")
+
+vm.name = "Tom"
 
 
 
